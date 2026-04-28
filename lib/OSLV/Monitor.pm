089be9a7bf4b9b808903d2fbe90864d514894ed8
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2026-04-28 13:49:39 -0500

    version bump, note changes, and ready for release

--- lib/OSLV/Monitor.pm
+++ lib/OSLV/Monitor.pm
@@ -11,11 +11,11 @@ OSLV::Monitor - OS level virtualization monitoring extend for LibreNMS.
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.0.3
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 
 =head1 SYNOPSIS
 
